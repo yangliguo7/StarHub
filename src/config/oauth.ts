d4d6ae33bcbeb3745184ac31373ef026a2835c1f
liguo.yang <liguo.yang@amh-group.com>
2026-05-01 13:45:01 +0800

    feat(version): update

--- src/config/oauth.ts
+++ src/config/oauth.ts
@@ -1,9 +1,9 @@
 /**
  * GitHub OAuth 配置
- * 
+ *
  * GitHub OAuth 必须要有 Client ID（这是 GitHub 的安全要求）
  * Client ID 是公开的，可以安全地放在代码中
- * 
+ *
  * 获取方式：
  * 1. 访问 https://github.com/settings/developers
  * 2. 点击 "New OAuth App"
@@ -15,6 +15,6 @@
  */
 
 export const GITHUB_OAUTH_CONFIG = {
-  CLIENT_ID: 'Ov23liIm4iNdpnHwGLfp' // 在这里填入你的 GitHub OAuth Client ID
+  CLIENT_ID: 'Ov23li69zEKBNJNpLRcQ' // 在这里填入你的 GitHub OAuth Client ID
 }
 
